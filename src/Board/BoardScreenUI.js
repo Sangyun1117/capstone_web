e8e46080c1437ee49e984ea340565303bb678084
azsaii <socczx@naver.com>
2024-04-02 10:19:05 +0900

    test board

--- src/Board/BoardScreenUI.js
+++ src/Board/BoardScreenUI.js
@@ -5,20 +5,6 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 90%;
-  width: 60%;
-  min-width: 30em;
-  background-color: #bbd2ec;
-  top: 10em;
-  left: 20%;
-  position: fixed;
-`;
-
 const TopContainer = styled.div`
   position: flex;
   flex-direction: row;
@@ -53,13 +39,139 @@ const WriteButton = styled.button`
   color: #fff;
   border-radius: 5px;
   transition: all 0.2s;
-  background-color: #5dc8cd;
+  background-color: #4dccc6;
   box-shadow: 0px 0px 0px 0px #01939a;
   border: none;
   font-size: 2em;
   z-index: 1;
   &:hover {
     box-shadow: 0px 0px 0px 5px #01939a;
+  }
+`;
+
+const BodyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 90%;
+  width: 60%;
+  min-width: 30em;
+  background-color: #bbd2ec;
+  top: 10em;
+  left: 20%;
+  position: fixed;
+`;
+
+const PostList = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const ListItem = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
+  width: 95%;
+  height: 50px;
+  top: 20px;
+  padding: 10px 25px;
+  margin: 10px;
+  border-radius: 5px;
+  border: none;
+  font-weight: 500;
+  background: transparent;
+  background-color: #4dccc6;
+  background-image: linear-gradient(315deg, #4dccc6 0%, #96e4df 74%);
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+  outline: none;
+  &:before,
+  &:after {
+    position: absolute;
+    content: '';
+    right: 0;
+    top: 0;
+    background: rgba(2, 126, 251, 1);
+    transition: all 0.3s ease;
+  }
+
+  &:before {
+    height: 0%;
+    width: 2px;
+  }
+
+  &:after {
+    width: 0%;
+    height: 2px;
+  }
+
+  &:hover {
+    background: transparent;
+    box-shadow: none;
+  }
+
+  &:hover:before {
+    height: 100%;
+  }
+
+  &:hover:after {
+    width: 100%;
+  }
+
+  & div {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    transition: all 0.3s ease;
+
+    &:hover {
+      color: rgba(2, 126, 251, 1);
+    }
+  }
+
+  & div:before,
+  & div:after {
+    position: absolute;
+    content: '';
+    left: 0;
+    bottom: 0;
+    background: rgba(2, 126, 251, 1);
+    transition: all 0.3s ease;
+  }
+
+  & div:before {
+    width: 2px;
+    height: 0%;
+  }
+
+  & div:after {
+    width: 0%;
+    height: 2px;
+  }
+
+  & div:hover:before {
+    height: 100%;
+  }
+
+  & div:hover:after {
+    width: 100%;
   }
 `;
 
@@ -112,7 +224,7 @@ const BoardScreenUI = ({ boardName }) => {
       <TopContainer>
         <CommentInput
           type="text"
-          placeholder="글 검색"
+          placeholder="검색하기"
           onChange={(e) => setSearch(e.target.value)}
         />
         <WriteButton
@@ -127,41 +239,41 @@ const BoardScreenUI = ({ boardName }) => {
           +
         </WriteButton>
       </TopContainer>
-      <Container>
+      <BodyContainer>
         {isLoading ? (
           <HashLoader loading={isLoading} size={50} />
         ) : (
           <>
-            <div>
-              <div className="postList">
-                {filteredPosts.map((item, index) => (
-                  <div
-                    key={item.id}
-                    style={{
-                      backgroundColor: index % 2 === 0 ? '#f8f8f8' : '#e8e8e8',
-                    }}
-                    onClick={() =>
-                      navigate('/postDetail', {
-                        post: item,
-                        boardName: boardName,
-                      })
-                    }
-                  >
-                    <div className="cardContent">
-                      <h3>
-                        {item.title.length > 20
-                          ? `${item.title.substring(0, 20)}..`
-                          : item.title}
-                      </h3>
-                      <p>{item.userEmail.split('@')[0]}</p>
-                    </div>
+            <PostList>
+              {filteredPosts.map((item, index) => (
+                <ListItem
+                  key={item.id}
+                  style={{
+                    backgroundColor: index % 2 === 0 ? '#f8f8f8' : '#e8e8e8',
+                  }}
+                  onClick={() =>
+                    navigate('/postDetail', {
+                      post: item,
+                      boardName: boardName,
+                    })
+                  }
+                >
+                  <div>
+                    <h3 style={{ marginLeft: '10px' }}>
+                      {item.title.length > 20
+                        ? `${item.title.substring(0, 20)}..`
+                        : item.title}
+                    </h3>
+                    <p style={{ marginLeft: '10px' }}>
+                      {item.userEmail.split('@')[0]}
+                    </p>
                   </div>
-                ))}
-              </div>
-            </div>
+                </ListItem>
+              ))}
+            </PostList>
           </>
         )}
-      </Container>
+      </BodyContainer>
     </>
   );
 };
